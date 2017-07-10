e93ca37ce15a35631879c7e9a547b642b0efb43a
Prigovor <prigovors@gmail.com>
2017-07-10 23:45:02 +0300

    FirstCommit

---
+++ src/main/java/hello/HelloWorldConfiguration.java
@@ -0,0 +1,18 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Prigovor on 10.07.2017.
+ */
+
+
+
+@SpringBootApplication
+public class HelloWorldConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldConfiguration.class, args);
+    }
+}
